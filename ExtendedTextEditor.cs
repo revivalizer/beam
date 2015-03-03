f87828c8224b071b41bf5377c42ac3ad5a858ae6
Ralph Brorsen <ralph@revivalizer.dk>
2015-03-03 23:27:52 +0100

    Copy basic editing over from glimmer.

---
+++ ExtendedTextEditor.cs
@@ -0,0 +1,196 @@
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Rendering;
+using ICSharpCode.AvalonEdit.Utils;
+using ICSharpCode.AvalonEdit.Editing;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Threading;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Drawing;
+using System.Windows.Media;
+
+namespace beam
+{
+    public class ExtendedTextEditor : TextEditor, INotifyPropertyChanged
+    {
+        public ExtendedTextEditor()
+        {
+            this.Background = new SolidColorBrush(Color.FromArgb(255, 0, 43, 54)); // base 03
+            this.Foreground = new SolidColorBrush(Color.FromArgb(255, 131, 148, 150)); // base 0
+
+            this.ShowLineNumbers = true;
+            this.Padding = new System.Windows.Thickness(5.0);
+            this.LineNumbersForeground = new SolidColorBrush(Color.FromArgb(255, 101, 123, 131)); // base 0
+        }
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		#region enum Mode
+		enum Mode
+		{
+            None, // No drag
+            Drag, // Dragging number
+		}
+
+        Mode mode = Mode.None;
+		#endregion
+
+        string matchedNumStr = "";
+        int matchedNumOffset = -1;
+        int matchedNumLength = 0;
+        bool matchedNumSign = false;
+        bool matchedNumDot = false;
+        int matchedNumFracDigits = 0;
+
+        int curLength;
+
+        bool hasMatch = false;
+
+        Point startDragPos;
+        double startDragValue;
+
+		object undoGroupDescriptor;
+
+        override protected void OnPreviewMouseMove(MouseEventArgs e)
+        {
+			if (e.Handled)
+				return;
+
+            if (mode == Mode.None)
+            {
+                TextViewPosition? position = this.TextArea.TextView.GetPositionFloor(e.GetPosition(this.TextArea.TextView) + this.TextArea.TextView.ScrollOffset);
+
+                if (position != null)
+                {
+                    DocumentLine line = this.TextArea.Document.GetLineByNumber(position.Value.Line); ;
+
+                    int begin = line.Offset;
+                    int end = line.EndOffset;
+                    int offset = this.TextArea.Document.GetOffset(position.Value.Location);
+                    int lineOffset = offset - begin;
+
+                    hasMatch = false;
+
+                    //if (Char.IsDigit(this.TextArea.Document.GetCharAt(offset)))
+                    {
+                        string l = this.TextArea.Document.GetText(line.Offset, line.Length);
+                        //string pat = @"([-+]?)\d+(\.?)(d*)";
+                        string pat = @"([-+]?)\d*(?:(\.)(\d*))?";
+                        Regex r = new Regex(pat);
+
+                        Match m = r.Match(l);
+
+                        while (m.Success && hasMatch == false)
+                        {
+                            int start = m.Index;
+                            int length = m.Length;
+
+                            if (start <= lineOffset && lineOffset < start + length)
+                            {
+                                matchedNumStr = m.Value;
+                                matchedNumOffset = start + begin;
+                                matchedNumLength = length;
+                                matchedNumSign = m.Groups[1].Value.Length > 0;
+                                matchedNumDot = m.Groups[2].Value.Length > 0;
+                                matchedNumFracDigits = m.Groups[3].Value.Length;
+                                curLength = matchedNumLength;
+
+                                hasMatch = true;
+
+                                undoGroupDescriptor = new object();
+                                this.Document.UndoStack.StartUndoGroup(undoGroupDescriptor);
+                                this.Document.UndoStack.EndUndoGroup();
+                            }
+
+                            m = m.NextMatch();
+                        }
+                    }
+                }
+            }
+            else if (mode == Mode.Drag)
+            {
+                Vector delta = e.GetPosition(this.TextArea) - startDragPos;
+                double dist = delta.X - delta.Y;
+
+                double newValue = startDragValue * Math.Exp(dist * 0.001);
+
+                string formatStr = "F" + matchedNumFracDigits.ToString();
+                string signStr = (matchedNumSign && newValue >= 0) ? "+" : "";
+                string numStr = signStr + newValue.ToString(formatStr, CultureInfo.InvariantCulture);
+
+                this.Document.UndoStack.StartContinuedUndoGroup(undoGroupDescriptor);
+                this.Document.Replace(matchedNumOffset, curLength, numStr);
+                this.Document.UndoStack.EndUndoGroup();
+                curLength = numStr.Length;
+
+                ICommand curSaveCommand = ((FileViewModel)this.TextArea.DataContext).SaveCommand;
+
+                // disabled because IsDirty flag is only set first change
+                // this is possible due to the continuing Undo group
+                //if (curSaveCommand.CanExecute(null))
+                    curSaveCommand.Execute(null);
+
+                e.Handled = true;
+            }
+        }
+
+		#region MouseLeftButtonDown
+		override protected void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+		{
+            if (e.Handled==true)
+                return;
+
+            FileViewModel curFile = ((FileViewModel)this.TextArea.DataContext);
+
+            if (hasMatch && curFile.IsDirty==false)
+            {
+                if (this.TextArea.CaptureMouse())
+                {
+                    mode = Mode.Drag;
+
+                    startDragPos = e.GetPosition(this.TextArea.TextView);
+                    startDragValue = Double.Parse(matchedNumStr, CultureInfo.InvariantCulture);
+                }
+
+                e.Handled = true;
+                return;
+            }
+		}
+		#endregion
+
+		#region MouseLeftButtonUp
+		override protected void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+            if (mode == Mode.None || e.Handled == true)
+                return;
+
+            if (mode == Mode.Drag)
+            {
+                this.TextArea.ReleaseMouseCapture();
+
+                mode = Mode.None;
+
+                e.Handled = true;
+                return;
+            }
+		}
+		#endregion
+
+    }
+}
---
+++ FileViewModel.cs
@@ -0,0 +1,286 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Utils;
+using ICSharpCode.AvalonEdit.Highlighting;
+
+namespace beam
+{
+    class FileViewModel : PaneViewModel
+    {
+        static int fileCount = 0;
+
+        #region Constructors
+        public FileViewModel(string filePath)
+        {
+          FilePath = filePath;
+          Load(filePath);
+          base.Title = Title;
+        }
+
+        public FileViewModel()
+        {
+            fileCount = fileCount + 1;
+            base.Title = "New File " + fileCount;
+
+            //this.HighlightDef = HighlightingManager.Instance.GetDefinition("GLSL");
+
+            IsDirty = true;
+        }
+
+        private void Load(string path)
+        {
+          if (File.Exists(path))
+          {
+            this._document = new TextDocument();
+            //this.HighlightDef = HighlightingManager.Instance.GetDefinition("GLSL");
+            this._isDirty = false;
+            //this.IsReadOnly = false;
+            //this.ShowLineNumbers = false;
+            //this.WordWrap = false;
+
+            // Check file attributes and set to read-only if file attributes indicate that
+            if ((System.IO.File.GetAttributes(path) & FileAttributes.ReadOnly) != 0)
+            {
+              this.IsReadOnly = true;
+              this.IsReadOnlyReason = "This file cannot be edit because another process is currently writting to it.\n" +
+                                      "Change the file access permissions or save the file in a different location if you want to edit it.";
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+              using (StreamReader reader = FileReader.OpenStream(fs, Encoding.UTF8))
+              {
+                this._document = new TextDocument(reader.ReadToEnd());
+              }
+            }
+          }
+
+        }
+        #endregion
+
+        #region FilePath
+        private string _filePath = null;
+        public string FilePath
+        {
+          get { return _filePath; }
+          set
+          {
+            if (_filePath != value)
+            {
+              _filePath = value;
+              ContentId = _filePath;
+
+              RaisePropertyChanged("FilePath");
+              RaisePropertyChanged("FileName");
+              RaisePropertyChanged("Title");
+            }
+          }
+        }
+        #endregion
+
+        #region FileName
+        public string FileName
+        {
+          get
+          {
+            if (FilePath == null)
+              return base.Title;
+
+            return System.IO.Path.GetFileName(FilePath);
+          }
+        }
+        #endregion FileName
+
+        #region Title
+        new public string Title
+        {
+          get
+          {
+            return FileName + (this.IsDirty == true ? "*" : string.Empty);
+          }
+
+          set
+          {
+            base.Title = value;
+          }
+        }
+        #endregion
+
+        #region Document
+        private TextDocument _document = null;
+        public TextDocument Document
+        {
+            get { return this._document; }
+            set
+            {
+                if (this._document != value)
+                {
+                    this._document = value;
+                    RaisePropertyChanged("Document");
+                    IsDirty = true;
+                }
+            }
+        }
+        #endregion
+
+        #region IsDirty
+        private bool _isDirty = false;
+        public bool IsDirty
+        {
+            get { return _isDirty; }
+            set
+            {
+                if (_isDirty != value)
+                {
+                    _isDirty = value;
+                    RaisePropertyChanged("IsDirty");
+                    RaisePropertyChanged("Title");
+                }
+            }
+        }
+        #endregion
+
+        #region IsReadOnly
+        private bool mIsReadOnly = false;
+        public bool IsReadOnly
+        {
+          get
+          {
+            return this.mIsReadOnly;
+          }
+
+          protected set
+          {
+            if (this.mIsReadOnly != value)
+            {
+              this.mIsReadOnly = value;
+              this.RaisePropertyChanged("IsReadOnly");
+            }
+          }
+        }
+
+        private string mIsReadOnlyReason = string.Empty;
+        public string IsReadOnlyReason
+        {
+          get
+          {
+            return this.mIsReadOnlyReason;
+          }
+
+          protected set
+          {
+            if (this.mIsReadOnlyReason != value)
+            {
+              this.mIsReadOnlyReason = value;
+              this.RaisePropertyChanged("IsReadOnlyReason");
+            }
+          }
+        }
+        #endregion IsReadOnly
+
+        #region SaveCommand
+        RelayCommand _saveCommand = null;
+        public ICommand SaveCommand
+        {
+          get
+          {
+            if (_saveCommand == null)
+            {
+              _saveCommand = new RelayCommand((p) => OnSave(p), (p) => CanSave(p));
+            }
+
+            return _saveCommand;
+          }
+        }
+
+        private bool CanSave(object parameter)
+        {
+          return IsDirty;
+        }
+
+        private void OnSave(object parameter)
+        {
+          Workspace.This.Save(this, false);
+        }
+
+        #endregion
+
+        #region SaveAsCommand
+        RelayCommand _saveAsCommand = null;
+        public ICommand SaveAsCommand
+        {
+          get
+          {
+            if (_saveAsCommand == null)
+            {
+              _saveAsCommand = new RelayCommand((p) => OnSaveAs(p), (p) => CanSaveAs(p));
+            }
+
+            return _saveAsCommand;
+          }
+        }
+
+        private bool CanSaveAs(object parameter)
+        {
+          return true;
+        }
+
+        private void OnSaveAs(object parameter)
+        {
+          Workspace.This.Save(this, true);
+        }
+        #endregion
+
+        #region CloseCommand
+        RelayCommand _closeCommand = null;
+        public ICommand CloseCommand
+        {
+          get
+          {
+            if (_closeCommand == null)
+            {
+              _closeCommand = new RelayCommand((p) => OnClose(), (p) => CanClose());
+            }
+
+            return _closeCommand;
+          }
+        }
+
+        private bool CanClose()
+        {
+          return true;
+        }
+
+        private void OnClose()
+        {
+          Workspace.This.Close(this);
+        }
+        #endregion
+
+        #region HighlightingDefinition
+        private IHighlightingDefinition _highlightdef = null;
+        public IHighlightingDefinition HighlightDef
+        {
+          get { return this._highlightdef; }
+          set
+          {
+            if (this._highlightdef != value)
+            {
+              this._highlightdef = value;
+              RaisePropertyChanged("HighlightDef");
+              IsDirty = true;
+            }
+          }
+        }
+        #endregion
+    }
+}
